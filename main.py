d8ed6ad56406d473d3dbb50884c2c582de6bbad4
Shiva <shiva1998r@gmail.com>
2021-12-26 18:20:24 +0530

    update readme

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import voice_assistant
 window=tk.Tk()
 window.title('Desktop Assistant')
 
-window.rowconfigure([0,1],minsize=100,weight=1)
+window.rowconfigure(0,minsize=100,weight=1)
 window.columnconfigure(0,minsize=200,weight=1)
 
 btns_frame=tk.Frame(master=window,relief=tk.RAISED, bd=3)
@@ -17,14 +17,9 @@ btn_open_apps=tk.Button(master=btns_frame,text='Open Applications',command= voic
 btn_voice_ass.grid(row=0,column=0)
 btn_open_apps.grid(row=0,column=1)
 
-
-
-lbl_help = tk.Label(text='Instructions...')
-
 btns_frame.grid(row=0, column=0,sticky='nsew')
-lbl_help.grid(row=1,column=0)
 
-height=300
-width=400
+height=200
+width=300
 window.geometry(f'{width}x{height}')
 window.mainloop()
