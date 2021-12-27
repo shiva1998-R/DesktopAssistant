766079d9f3188dca9dbf46374e773503fa76c833
Shiva <shiva1998r@gmail.com>
2021-12-27 20:47:36 +0530

    Add requirements file and generate exe

--- main.py
+++ main.py
@@ -3,7 +3,159 @@ import os
 import shutil
 import os.path
 from tkinter.filedialog import askdirectory, askopenfile, askopenfilename
-import voice_assistant
+
+# voice assistant module
+from tkinter import *
+from sys import exit
+
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import pyjokes
+import random
+import requests
+import datetime
+
+
+import threading
+
+# utilities for voice assistant
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    """
+    Speaks audio
+    """
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def command():
+    """
+    Listens to commands
+    """
+    r = sr.Recognizer()
+    while True:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source,duration=2)
+            speak('Alexa: Listening...')
+            audio=r.listen(source,phrase_time_limit=5)
+            try:
+                query = r.recognize_google(audio)
+                speak(f"You said: {query}")
+                return query
+            except:
+                speak("Try Again")
+
+
+def clickVoiceAssistantButton():
+    def VoiceAssistantButtonActions():
+        while True:
+            query = command().lower()  ## takes user command
+
+            if 'name' in query:
+                speak("Hello Machine! My  Name is Alexa")
+            elif 'are you single' in query:
+                answers = ['I am in a relationship with wifi',
+                            'No, I love spending time thinking about my crush wifi']
+                speak(random.choice(answers))
+            elif 'hate' in query:
+                speak("I hate when people called me a machine")
+            elif 'love' in query:
+                speak("I loves to chat with machines like you")
+            ### time
+            elif 'time' in query:
+                time = datetime.datetime.now().strftime('%I:%M %p')
+                speak(f"It's {time} master")
+
+            ### celebrity
+            elif 'who is' in query:
+                query = query.replace('who is', "")
+                speak(wikipedia.summary(query, 2))
+
+            ### Joke
+            elif 'joke' in query:
+                speak(pyjokes.get_joke())
+
+            ### news
+            elif 'news' in query:
+                def trndnews():
+                    url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=59ff055b7c754a10a1f8afb4583ef1ab"
+                    page = requests.get(url).json()
+                    article = page["articles"]
+                    results = []
+                    for ar in article:
+                        results.append(ar["title"])
+
+                    results = results[:3]
+                    speak("here are the top trending news....!!")
+                    speak("Do yo want me to read!!!")
+                    reply = command().lower()
+                    reply = str(reply)
+                    if reply == "yes":
+                        speak(results)
+                    else:
+                        speak('ok!!!!')
+                        pass
+
+                trndnews()
+
+
+            ### music
+            elif 'music' in query:
+                with open(r'C://Desktop Assistant/music_folder_location.txt') as f:
+                    music_dir = f.read()
+                songs = os.listdir(music_dir)
+                song = random.randint(0, len(songs) - 1)
+                print(songs[song])
+                speak(f"playing {songs[song]}")
+                os.startfile(os.path.join(music_dir, songs[0]))
+
+            elif "exit" in query:
+                speak("Have a nice day ! ")
+                break
+            else:
+                speak("I don't understand what you are saying")
+    t=threading.Thread(target=VoiceAssistantButtonActions)
+    t.daemon=True
+    t.start()
+
+def clickOpenApplicationsButton():
+
+    speak('Opening Applications...')
+    def OpenAppsActions():
+        import subprocess
+        with open(r'C://Desktop Assistant/app_exe_locations.txt') as f:
+            lines=f.readlines()
+            lines=[line.strip() for line in lines]
+            for exe_location in lines:
+                subprocess.Popen(exe_location)
+    t=threading.Thread(target=OpenAppsActions)
+    t.daemon = True
+    t.start()
+
+
+def clickMusicPlayerButton():
+
+    speak('Playing music...')
+    def PlayMusicActions():
+        with open(r'C://Desktop Assistant/music_folder_location.txt') as f:
+              music_dir = f.read()
+        songs = os.listdir(music_dir)
+        song = random.randint(0, len(songs) - 1)
+        print(songs[song])
+        speak(f"playing {songs[song]}")
+        os.startfile(os.path.join(music_dir, songs[0]))
+    t=threading.Thread(target=PlayMusicActions)
+    t.daemon = True
+    t.start()
+
+
+
+
 
 
 
@@ -107,9 +259,9 @@ btns_frame.rowconfigure(0,weight=1)
 btns_frame.columnconfigure(list(range(3)),weight=1)
 
 # Adding buttons
-btn_voice_ass = tk.Button(master=btns_frame,text='Voice Assistant 🎤',command= voice_assistant.clickVoiceAssistantButton,font=("TkDefaultFont", 15))
-btn_open_apps=tk.Button(master=btns_frame,text='Open Applications',command= voice_assistant.clickOpenApplicationsButton,font=("TkDefaultFont", 15))
-btn_music_player=tk.Button(master=btns_frame,text='Play music',command= voice_assistant.clickMusicPlayerButton,font=("TkDefaultFont", 15))
+btn_voice_ass = tk.Button(master=btns_frame,text='Voice Assistant 🎤',command= clickVoiceAssistantButton,font=("TkDefaultFont", 15))
+btn_open_apps=tk.Button(master=btns_frame,text='Open Applications',command= clickOpenApplicationsButton,font=("TkDefaultFont", 15))
+btn_music_player=tk.Button(master=btns_frame,text='Play music',command= clickMusicPlayerButton,font=("TkDefaultFont", 15))
 
 # Placing buttons
 btn_voice_ass.grid(row=0,column=0)
